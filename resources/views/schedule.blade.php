8fe5c76d7fa97e32d1e0f6196082580261c896bb
Eliton Campos <eliton.pecas@gmail.com>
2023-07-30 23:12:58 -0300

    Test Auth Git

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto px-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <p>Agenda Foda</p>
+                    <p>Agenda</p>
                 </div>
             </div>
         </div>
