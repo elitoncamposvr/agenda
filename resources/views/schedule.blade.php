e33739a0f32eb3d49b591500f30b895402d70965
Eliton Campos <eliton.pecas@gmail.com>
2023-07-30 22:57:58 -0300

    Implemented Menu

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto px-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div><span class="font-semibold">Olá,  </span> {{ Auth::user()->name }}.</div>
+                    <p>Agenda</p>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScheduleController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::redirect('/', '/dashboard');
 
@@ -24,6 +14,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+Route::get('/schedule', [ScheduleController::class, 'index'])->name('schedule');
 });
 
 require __DIR__.'/auth.php';
